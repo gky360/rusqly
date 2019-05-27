05b753b2740622306987bf7496f172b9b498651d
gky360 <gky360@gmail.com>
2019-05-28 03:31:48 +0900

    create cache dir if not exists

--- src/lib.rs
+++ src/lib.rs
@@ -4,25 +4,47 @@ extern crate failure;
 extern crate lazy_static;
 
 use dirs::home_dir;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 use std::path::PathBuf;
-use std::result;
+use std::{fs, io, result};
 use structopt::StructOpt;
 
 pub type Result<T> = result::Result<T, Error>;
 
-#[derive(Fail, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Fail, Debug)]
 pub enum Error {
+    #[fail(display = "{}", _0)]
+    Io(io::Error),
+    #[fail(display = "{}", _0)]
+    Readline(ReadlineError),
     #[fail(display = "Unknown")]
     Unknown,
 }
 
+impl From<ReadlineError> for Error {
+    fn from(err: ReadlineError) -> Self {
+        Error::Readline(err)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::Io(err)
+    }
+}
+
 lazy_static! {
-    static ref DEFAULT_HISTORY: PathBuf = {
+    static ref CACHE_DIR: String = {
         let mut path = home_dir().unwrap();
         path.push(".cache");
         path.push("rusqly");
+        String::from(path.to_str().unwrap())
+    };
+    static ref DEFAULT_HISTORY_PATH: String = {
+        let mut path = PathBuf::from(&CACHE_DIR as &str);
         path.push("history.txt");
-        path
+        String::from(path.to_str().unwrap())
     };
 }
 
@@ -30,15 +52,32 @@ lazy_static! {
 #[structopt()]
 pub struct Opt {
     /// path to history file
-    #[structopt(
-        long,
-        parse(from_os_str),
-        raw(default_value = "DEFAULT_HISTORY.to_str().unwrap()")
-    )]
+    #[structopt(long, parse(from_os_str), raw(default_value = "&DEFAULT_HISTORY_PATH"))]
     pub history: PathBuf,
 }
 
 pub fn run(opt: &Opt) -> Result<()> {
-    println!("{:?}", opt);
+    eprintln!("{:?}", opt);
+
+    // create cache dir if not exists
+    fs::create_dir_all(&CACHE_DIR as &str)?;
+
+    let mut rl = Editor::<()>::new();
+    let _ = rl.load_history(&opt.history);
+    loop {
+        let readline = rl.readline("rusqly> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                eprintln!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
+            Err(err) => return Err(err.into()),
+        }
+        rl.save_history(&opt.history)?;
+    }
+
+    println!("Bye.");
+
     Ok(())
 }
